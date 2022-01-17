76f149beb8bf05c2cfc9cfab9630b76822886106
HarleyAppleChoi <appleno56@gmail.com>
2022-01-17 17:12:55 +0000

    Add account related endpoints

--- modules/auth/utils/newBlocks.go
+++ modules/auth/utils/newBlocks.go
@@ -29,6 +29,7 @@ func GetNewAccounts(client client.Proxy) ([]types.Account, error) {
 	err := client.RestRequestGetDecoded("accounts", txsParams, &addresses)
 
 	accounts:=make([]types.Account,25)
+
 	for i,a:=range addresses{
 		fmt.Println(fmt.Sprintf("Address:%s",a.Address))
 		var account types.Account
@@ -37,9 +38,46 @@ func GetNewAccounts(client client.Proxy) ([]types.Account, error) {
 			return nil,err
 		}
 		accounts[i]=account
+
 	}
 	if err != nil {
 		return nil, err
 	}
 	return accounts, nil
+}
+
+func getScResults(address string,client client.Proxy) ([]types.SCResult,error){
+	var scResult []types.SCResult
+	err := client.RestRequestGetDecoded(fmt.Sprintf("accounts/%s/sc-results",address), nil, &scResult)
+	if err!=nil{
+		return nil,err
+	}
+	return scResult,nil
+}
+
+func getTokens(address string,client client.Proxy) ([]types.Token,error){
+	var scResult []types.Token
+	err := client.RestRequestGetDecoded(fmt.Sprintf("accounts/%s/tokens",address), nil, &scResult)
+	if err!=nil{
+		return nil,err
+	}
+	return scResult,nil
+}
+
+func getNFTs(address string,client client.Proxy) ([]types.NFT,error){
+	var nfts []types.NFT
+	err := client.RestRequestGetDecoded(fmt.Sprintf("accounts/%s/nfts?includeFlagged=true",address), nil, &nfts)
+	if err!=nil{
+		return nil,err
+	}
+	return nfts,nil
+}
+
+func getContracts(address string,client client.Proxy) ([]types.NFT,error){
+	var nfts []types.NFT
+	err := client.RestRequestGetDecoded(fmt.Sprintf("accounts/%s/contracts",address), nil, &nfts)
+	if err!=nil{
+		return nil,err
+	}
+	return nfts,nil
 }
--- types/auth.go
+++ types/auth.go
@@ -1,12 +1,99 @@
 package types
 
 type Account struct {
-	Address         string `json:"address"`
-	Nonce           int    `json:"nonce"`
-	Balance         string `json:"balance"`
-	RootHash        string `json:"rootHash"`
-	TxCount         int    `json:"txCount"`
-	ScrCount        int    `json:"scrCount"`
-	Shard           int    `json:"shard"`
-	DeveloperReward string `json:"developerReward"`
+	Address  string `json:"address"`
+	Balance  string `json:"balance"`
+	Nonce    string `json:"nonce"`
+	Shard    int    `json:"shard"`
+	ScamInfo struct {
+	} `json:"scamInfo"`
+	Code                     string `json:"code"`
+	CodeHash                 string `json:"codeHash"`
+	RootHash                 string `json:"rootHash"`
+	TxCount                  int    `json:"txCount"`
+	ScrCount                 int    `json:"scrCount"`
+	Username                 string `json:"username"`
+	DeveloperReward          string `json:"developerReward"`
+	OwnerAddress             string `json:"ownerAddress"`
+	DeployedAt               int    `json:"deployedAt"`
+	IsUpgradeable            bool   `json:"isUpgradeable"`
+	IsReadable               bool   `json:"isReadable"`
+	IsPayable                bool   `json:"isPayable"`
+	IsPayableBySmartContract bool   `json:"isPayableBySmartContract"`
+}
+
+type SCResult struct {
+	Hash           string `json:"hash"`
+	Timestamp      int    `json:"timestamp"`
+	Nonce          int    `json:"nonce"`
+	GasLimit       int    `json:"gasLimit"`
+	GasPrice       int    `json:"gasPrice"`
+	Value          string `json:"value"`
+	Sender         string `json:"sender"`
+	Receiver       string `json:"receiver"`
+	RelayedValue   string `json:"relayedValue"`
+	Data           string `json:"data"`
+	PrevTxHash     string `json:"prevTxHash"`
+	OriginalTxHash string `json:"originalTxHash"`
+	CallType       string `json:"callType"`
+	Logs           struct {
+		Address string   `json:"address"`
+		Events  []string `json:"events"`
+	} `json:"logs"`
+	ReturnMessage struct {
+	} `json:"returnMessage"`
+}
+
+type Token struct {
+	Identifier     string `json:"identifier"`
+	Name           string `json:"name"`
+	Ticker         string `json:"ticker"`
+	Owner          string `json:"owner"`
+	Minted         string `json:"minted"`
+	Burnt          string `json:"burnt"`
+	Decimals       int    `json:"decimals"`
+	IsPaused       bool   `json:"isPaused"`
+	CanUpgrade     bool   `json:"canUpgrade"`
+	CanMint        bool   `json:"canMint"`
+	CanBurn        bool   `json:"canBurn"`
+	CanChangeOwner bool   `json:"canChangeOwner"`
+	CanPause       bool   `json:"canPause"`
+	CanFreeze      bool   `json:"canFreeze"`
+	CanWipe        bool   `json:"canWipe"`
+	Balance        string `json:"balance"`
+}
+
+type NFT struct {
+	Identifier string   `json:"identifier"`
+	Collection string   `json:"collection"`
+	Attributes string   `json:"attributes"`
+	Nonce      int      `json:"nonce"`
+	Type       string   `json:"type"`
+	Name       string   `json:"name"`
+	Creator    string   `json:"creator"`
+	Royalties  int      `json:"royalties"`
+	Uris       []string `json:"uris"`
+	URL        string   `json:"url"`
+	Media      []struct {
+		URL          string `json:"url"`
+		OriginalURL  string `json:"originalUrl"`
+		ThumbnailURL string `json:"thumbnailUrl"`
+		FileType     string `json:"fileType"`
+		FileSize     int    `json:"fileSize"`
+	} `json:"media"`
+	IsWhitelistedStorage bool `json:"isWhitelistedStorage"`
+	Metadata             struct {
+	} `json:"metadata"`
+	ScamInfo struct {
+		Type string `json:"type"`
+		Info string `json:"info"`
+	} `json:"scamInfo"`
+	Balance string `json:"balance"`
+	Ticker  string `json:"ticker"`
+}
+
+type Contract struct {
+	Address      string `json:"address"`
+	DeployTxHash string `json:"deployTxHash"`
+	Timestamp    int    `json:"timestamp"`
 }
